3ccd10423157a2432cd1f3899f7f3b87c4bccba2
Vladlen Danilin <danilinwladyslaw@yandex.ru>
2023-12-23 15:18:41 +0400

    added IRepository

---
+++ Shared/Shared.Application/Common/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Application.Common
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IEnumerable<T> GetList();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+    }
+}
