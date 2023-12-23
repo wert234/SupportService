742952da76f86beb3210c0ed3c8b250a9dbcecd0
Vladlen Danilin <danilinwladyslaw@yandex.ru>
2023-12-23 14:58:00 +0400

    added Appeal

---
+++ Shared/Shared.Domain/Entitys/Appeal.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Domain.Enums;
+
+namespace Shared.Domain.Entitys
+{
+    public class Appeal
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public IEnumerable<Message> Messages { get; set; } = Enumerable.Empty<Message>();
+        public Status Status { get; set; } = Status.Creaded;
+    }
+}
