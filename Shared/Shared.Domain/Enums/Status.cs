3f31b8966f69ea55bd41cecf91edb1b077173bcc
Vladlen Danilin <danilinwladyslaw@yandex.ru>
2023-12-23 14:55:56 +0400

    Added Status appeal

---
+++ Shared/Shared.Domain/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Shared.Domain.Enums
+{
+    public enum Status
+    {
+        Creaded = 0,
+        OnTheFirstLine = 1,
+        OnTheSecondLine = 2,
+        OnTheThirdLine = 3,
+        Closed = 4,
+    }
+}
